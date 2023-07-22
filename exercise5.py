ce12bb03b66db74eece78358ba0761ee1e499527
UreshiiSushi <merieru@gmail.com>
2023-07-22 16:56:16 +0200

    Add three new exercises

---
+++ exercise5.py
@@ -0,0 +1,14 @@
+def fizzBuzz(upTo):
+    number = int(upTo)
+    for i in range(1, number+1):
+        if i%3 == 0 and i%5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i%3 == 0:
+            print("Fizz", end=" ")
+        elif i%5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(i, end=" ")
+
+x = input("Enter number for start game:\n")
+fizzBuzz(x)
